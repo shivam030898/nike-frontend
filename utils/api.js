9a0686eba6f014c101b8b896e9acaac8f2b24d88
shivam030898 <s.shivam7879@gmail.com>
2023-06-22 16:42:28 +0530

    Second

--- utils/api.js
+++ utils/api.js
@@ -4,8 +4,7 @@ export const fetchDataFromApi = async (endpoint) => {
   const options = {
     method: "GET",
     headers: {
-      Authorization:
-        "Bearer 41525a17f0225995d548b4deb27b9793b3db847dca0171b8dd75387da68a1c83f4eb131138a12607766bd075ed9d8fd41f1a8d02cd836c9a07ff1e1d6b1fe6c5ab657c139d54f3849cebc5f4a6f51a9f5f3bb83e5b5b055a9adc302d15513493fe2be06bae8f05da96cfe2b8f7d74cf2487a8afd2ab958aa25e6bcec9b0688e4",
+      Authorization: "Bearer " + STRAPI_API_TOKEN,
     },
   };
 
